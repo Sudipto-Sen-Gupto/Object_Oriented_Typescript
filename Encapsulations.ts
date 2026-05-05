e86256b1abebb394fb55a4516dde1bcde5478801
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2026-05-06 05:52:23 +0600

    the fourth piller of oop encapsulation

---
+++ Encapsulations.ts
@@ -0,0 +1,47 @@
+ 
+
+ class BankAccount{
+           public userName:string;
+         public  readonly   userId:string;
+            // private userBalance:number;
+            protected userBalance:number;
+
+            constructor(userName:string,userId:string,userBalance:number){
+                     this.userName=userName;
+                     this.userId=userId;
+                     this.userBalance=userBalance;
+
+            }
+
+            //by using private we can declare this object into this class 
+
+           private addBalance(balance:number){
+                this.userBalance=this.userBalance+balance
+            }
+
+            callBalance(balance:number){
+                 this.addBalance(balance)
+            }
+ }
+
+   const myBalance=new BankAccount('Gaurav','h144',10000)
+//    myBalance.userId='34h1' //thus we can not change this kind of value thats we should use access modifier as read only modifier
+// myBalance.userBalance=10000+5000; thus we can not do  this for this kind of sensitive information .If we set 0 for balance the rich people will poor and the bank manager will go jail .We can use 'private' keyword and make a function for balance increasing or decreasing;
+
+ class TomarAccount extends BankAccount{
+         addTomarBalance(balance:number){
+                                  //if we use private keyword then we cann't access userBalance in her so we can use protected keyword for our work purpose
+                      this.userBalance=this.userBalance+balance            
+                                   
+         }
+ }
+     
+ const TomarBalance=new TomarAccount('Piyali','dkf33',12200)
+  TomarBalance.addTomarBalance(5000)
+  console.log(TomarBalance);
+
+   console.log(myBalance.userId); 
+   myBalance.callBalance(5039);
+   console.log(myBalance);
+   
+      
