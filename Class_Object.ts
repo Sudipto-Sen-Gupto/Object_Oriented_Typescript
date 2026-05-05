7426e01c38d189bf352fd285dd30b9d8222df13b
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2026-05-05 22:26:54 +0600

    learn how to make a customized class and make object and method into it

--- Class_Object.ts
+++ Class_Object.ts
@@ -1,5 +1,23 @@
 
- type abc=string[]
+  class Vehicle{
+         name:string;
+         price:number;
+         roar:string;
+         performance:string;
 
- const array:abc=['apple','orange','papaya']
- console.log(array);
+         constructor(name:string,price:number,roar:string,performance:string){
+                  this.name=name;
+                  this.price=price;
+                  this.roar=roar;
+                  this.performance=performance;
+         }
+
+         vehiclePerform(){
+                    console.log(`${this.name} performance is ${this.performance}`);
+         }
+  }
+
+  const yahmaahaBike=new Vehicle('FzV1',270000,'ghir ghir ghir','Smooth and comfort')
+  const suzukiBike=new Vehicle('Suzuki zixxer',250000,'hoom hoo hoom','Rude and wonderful braking control')
+  console.log(suzukiBike.price);
+  yahmaahaBike.vehiclePerform()
